f8f2d73e091e53f493a171e03a629364d57d5031
Anil Upadhyay <capgemini.upadhyay@daimler.com>
2017-08-16 00:07:38 +0530

    add sqlite helper

---
+++ iOS_Subjective_Question2/LocationLogging/Helper/LocationDB.swift
@@ -0,0 +1,13 @@
+//
+//  LocationDB.swift
+//  LocationLogging
+//
+//  Created by Anil Upadhyay on 15/08/17.
+//  Copyright © 2017 Anil Upadhyay. All rights reserved.
+//
+
+import UIKit
+
+class LocationDB: NSObject {
+
+}
---
+++ iOS_Subjective_Question2/LocationLogging/Helper/SQLLiteHelper.swift
@@ -0,0 +1,13 @@
+//
+//  SQLLiteHelper.swift
+//  LocationLogging
+//
+//  Created by Anil Upadhyay on 15/08/17.
+//  Copyright © 2017 Anil Upadhyay. All rights reserved.
+//
+
+import UIKit
+
+class SQLLiteHelper: NSObject {
+
+}
